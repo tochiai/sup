8061702abb1910831ebad81a0fd2891386646ab8
Hack Reactor Students <accounts@hackreactor.com>
2014-06-12 16:05:41 -0700

    Correct grammar

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ var secondWord = ['G', 'homeslice', 'homeskillet', 'homie', 'brah', 'bro'];
 var firstChoice = firstWord[Math.floor(firstWord.length * Math.random())];
 var secondChoice = secondWord[Math.floor(secondWord.length * Math.random())];
 $(document).ready(function() {
-  $('.greeting').text(firstChoice + secondChoice);
+  $('.greeting').text(firstChoice + ' ' + secondChoice + '?');
 });
